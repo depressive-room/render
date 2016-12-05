630881203ae6202d1c5798edb000461eaed9df3c
AssirDaniilov <stm8725721@mail.ru>
2016-12-05 23:20:28 +0300

    Add files via upload

---
+++ src/Tagtable.cpp
@@ -0,0 +1,17 @@
+#include "Tagtable.h"
+#include <QtWidgets/QWidget>
+
+namespace Render
+{
+
+}
+
+TagTable::TagTable(const ParserNode & parserNode):
+         Base(parserNode)
+{
+
+}
+void TagTable::blabla()
+{
+    std::cout << "Table" << std::endl;
+}
---
+++ src/Tagtable.h
@@ -0,0 +1,19 @@
+#ifndef TAGTABLE_H
+#define TAGTABLE_H
+
+#include <parser.hpp>
+#include <QtWidgets/QWidget>
+
+namespace Render
+{
+
+}
+class TagTable: public QWidget
+{
+   public
+      Q_OBJECT
+      TagTable(const ParserNode &parserNode);
+      void blabla();
+};
+
+#endif
